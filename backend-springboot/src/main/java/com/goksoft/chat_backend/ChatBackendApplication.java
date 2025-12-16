c630bd97c72bf2b38f5fb692e52343f434c46add
jakobgokpinar <jakobgokpinar@gmail.com>
2025-12-16 13:40:35 +0100

    backend application.java

--- backend-springboot/src/main/java/com/goksoft/chat_backend/ChatBackendApplication.java
+++ backend-springboot/src/main/java/com/goksoft/chat_backend/ChatBackendApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ChatBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ChatBackendApplication.class, args);
+
+        SpringApplication.run(ChatBackendApplication.class, args);
 	}
 
 }
