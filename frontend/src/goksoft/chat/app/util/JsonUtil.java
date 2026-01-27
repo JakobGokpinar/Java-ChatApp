f83ea75f55fe952899c5bb042aca0aa51d5743de
jakobgokpinar <jakobgokpinar@gmail.com>
2026-01-27 19:49:09 +0100

    network-layer

---
+++ frontend/src/goksoft/chat/app/util/JsonUtil.java
@@ -0,0 +1,20 @@
+package goksoft.chat.app.util;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+public class JsonUtil {
+    private static final Gson gson = new Gson();
+    
+    public static <T> T fromJson(String json, Class<T> clazz) {
+        return gson.fromJson(json, clazz);
+    }
+    
+    public static <T> T fromJson(String json, TypeToken<T> typeToken) {
+        return gson.fromJson(json, typeToken.getType());
+    }
+    
+    public static String toJson(Object obj) {
+        return gson.toJson(obj);
+    }
+}
